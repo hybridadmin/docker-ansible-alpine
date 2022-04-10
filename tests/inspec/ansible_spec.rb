d8dd07eedadfcaadf388dc21105034e0bc704149
Tinashe Chikomo <tinashe@chikomo.net>
2022-04-10 22:21:28 +0200

    small change

--- tests/inspec/ansible_spec.rb
+++ tests/inspec/ansible_spec.rb
@@ -15,7 +15,7 @@ control 'ansible-01' do
       describe pip('ansible') do
         it { should be_installed }
       end
-      describe pip('ansible', '/usr/bin/pip3.6') do
+      describe pip('ansible', '/usr/bin/pip3.8') do
         it { should be_installed }
       end
       describe pip('ansible', 'python3 -m pip') do
